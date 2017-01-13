321db10fba0b390e644e31838ef72792c1798bbe
Max Fechner <max.fechner@gmail.com>
2017-01-13 22:12:08 +0800

    ...

--- map.js
+++ map.js
@@ -1,12 +1,12 @@
-addMarker = (map, lat, lon, url, color) => {
+addMarker = (map, lat, lon, url, icon) => {
   var marker = new google.maps.Marker({
     position: new google.maps.LatLng(lat, lon),
     map: map,
-    icon: `dot/${color}.12.png`
+    icon: `icons/${icon}.png`
   });
   marker.addListener('click', function() {
-    map.setZoom(8);
-    map.setCenter(marker.getPosition());
+    // map.setZoom(8);
+    // map.setCenter(marker.getPosition());
     showFrame(url);
   });
 
@@ -38,9 +38,28 @@ initMap = () => {
         styles: mapStyle
       });
 
-      addMarker(map, 11.181448, 119.386825, '//lit.max.pub/DE/', 'red');
-      addMarker(map, 9.463144, 123.380028, '//lit.max.pub/EN/', 'red');
-      addMarker(map, 11.153562, 123.809435, '//lit.max.pub/FR/', 'yellow');
+      fetch('loc.json')
+        .then((response) => response.json())
+        .then((places) => {
+          places.forEach((p) => {
+            addMarker(map, p.lat, p.lon, p.url, p.ico);
+          });
+        });
 
     });
 }
+
+
+
+// addMarker(map, 11.181448, 119.386825, '/PH/Bantayan', 'sport');
+// addMarker(map, 9.463144, 123.380028, '//lit.max.pub/EN/', 'sport');
+// addMarker(map, 11.153562, 123.809435, '//lit.max.pub/FR/', 'beach');
+// addMarker(map, 9.1999, 123.5952, '//lit.max.pub/FR/', 'beach');
+// addMarker(map, 9.1999, 123.5952, '//lit.max.pub/FR/', 'beach');
+// addMarker(map, 10.3107, 123.9802, '//lit.max.pub/FR/', 'air-port');
+// addMarker(map, 10.2933, 123.9070, '/PH/Bantayan', 'sea-port');
+
+// // bantayan
+// addMarker(map, 11.156796, 123.787647, '/PH/Bantayan', 'air-port');
+// addMarker(map, 11.156796, 123.787647, '/PH/Bantayan', 'sea-port');
+// addMarker(map, 11.156796, 123.787647, '/PH/Bantayan', 'sea-port');
